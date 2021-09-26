2a6ccf6350e04de537ea0c3b1bc7c289b8c43857
Zane Gates <u7334218@anu.edu.au>
2021-09-27 00:46:27 +1000

    Modified function call names in Viewer in alignment with refactoring of Die

--- src/comp1140/ass2/gui/Viewer.java
+++ src/comp1140/ass2/gui/Viewer.java
@@ -221,7 +221,7 @@ public class Viewer extends Application {
             this.die = die;
 
             // Apply the die texture to the mesh
-            setMaterial(die.isWhite() ? whiteMaterial : blackMaterial);
+            setMaterial(die.isDieWhite() ? whiteMaterial : blackMaterial);
 
             // Rotate the mesh to show the correct numbers
             getTransforms().add(necessaryRotations());
