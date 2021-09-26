74b075cf1b1a84e2b4d6042c042ede8ec7b2d451
Yuechen Liu <u7227895@anu.edu.au>
2021-09-27 02:36:08 +0930

    die to encoding test

---
+++ tests/comp1140/ass2/dieToEncodingTest.java
@@ -0,0 +1,49 @@
+package comp1140.ass2;
+
+
+import comp1140.ass2.State.Die;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Timeout;
+
+
+import static comp1140.ass2.State.Die.dieToEnc;
+import static java.util.concurrent.TimeUnit.MILLISECONDS;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@Timeout(value = 1000, unit = MILLISECONDS)
+    public class dieToEncodingTest {
+
+    @Test
+    public void whitePlayerDice() {
+
+        for (char zero = 'A'; zero < 'W'; zero++){
+            for(char one = 'a'; one<'g'; one++){
+                for(char two = '1'; two < '7'; two++){
+                    String die = ""+zero+one+two;
+                    Die dice = new Die(die, null, null);
+
+                    assertEquals(die, dieToEnc(dice), "Die with string \"" + die + " returns the string \"" + die + " which is valid.");
+
+                    }
+                }
+            }
+    }
+
+    @Test
+    public void blackPlayerDice() {
+
+        for (char zero = 'a'; zero < 'w'; zero++){
+            for(char one = 'a'; one<'g'; one++){
+                for(char two = '1'; two < '7'; two++){
+                    String die = ""+zero+one+two;
+                    Die dice = new Die(die, null, null);
+
+                    assertEquals(die, dieToEnc(dice), "Die with string \"" + die + " returns the string \"" + die + " which is valid.");
+
+                }
+            }
+        }
+    }
+
+    }
+
